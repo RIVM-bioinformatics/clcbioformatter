33fa26ec6b24349ad7a15e789c0c0d637e67a8a5
AleSR13 <ale.hdz.segura@gmail.com>
2022-01-14 16:48:04 +0100

    Files sent to cluster and waiting for them to finish

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,13 +6,12 @@ set -euo pipefail
 
 #----------------------------------------------#
 # User parameters
-input_dir="${1%/}"
-output_dir="${2%/}"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 cd ${DIR}
 
 # Sanity checks
+set +euo pipefail
 if [ ! -z "${1}" ] || [ ! -z "${2}" ]
 then
   INPUTDIR=$(realpath ${1})
@@ -21,19 +20,13 @@ else
   echo "Not enough arguments were provided. This script needs 2 arguments: input directory and output directory."
   exit 1
 fi
+set -euo pipefail
 
 if [ ! -d "${INPUTDIR}" ] || [ ! -d "${OUTPUTDIR}" ]
 then
   echo "Either the input directory (${INPUTDIR}) or the output directory (${OUTPUTDIR}) do not exist!"
   exit 1
 fi
-
-#----------------------------------------------#
-# Create/update necessary environments
-# PATH_MASTER_YAML="clcbioformatter.yaml"
-# MASTER_NAME=$(head -n 1 ${PATH_MASTER_YAML} | cut -f2 -d ' ')
-
-echo -e "\nUpdating necessary environments to run the pipeline..."
 
 #----------------------------------------------#
 # Run the pipeline
@@ -57,11 +50,27 @@ if [ ! -f ${sing_image} ]
 then
   singularity pull --name "${sing_image}" library://alesr13/default/clcbioformatter:sha256.3cf5a626bad956d5bc9e35cadd71ea4d5f509007002429e24ad887e9e5dd1dc0
 fi
-singularity exec \
+
+# Generate random job_id
+RANDOM=$(date +%s%N | cut -b10-19)
+job_id="clcbioformatter_${RANDOM}"
+
+mkdir -p "${OUTPUTDIR}/logs"
+
+bsub -J "${job_id}" \
+  -n 4 \
+  -q "${QUEUE}" \
+  -o "${OUTPUTDIR}/logs/clcbioformatter.out" \
+  -e "${OUTPUTDIR}/logs/clcbioformatter.err" \
+  -R \"span[hosts=1]\" \
+  -W 60 \
+  "singularity exec \
   --bind ${INPUTDIR}:${INPUTDIR} \
   --bind ${OUTPUTDIR}:${OUTPUTDIR} \
-  $"${sing_image}" \
-  python clcbioformatter/multifile_formatter.py -i ${INPUTDIR} -o ${OUTPUTDIR} -n 4
+  ${sing_image} \
+  python clcbioformatter/multifile_formatter.py -i ${INPUTDIR} -o ${OUTPUTDIR} -n 4"
+
+bwait -w "${job_id}"
     
 result=$?
 
@@ -93,6 +102,8 @@ if [ ${result} == 0 ]
 then
   echo -e "\n\nFinished successfully reformatting the fasta files from ${INPUTDIR}."
   echo -e "\nThe re-formatted files can be found at ${OUTPUTDIR}.\n"
+else
+  echo "An error occured while running the clcbioformatter"
 fi
 
 exit ${result}
